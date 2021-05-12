85aa505d538315682338811bc1f06825f5e90b3d
Sylvain Leclerc <sylvain.leclerc@rte-france.com>
2021-05-12 20:38:21 +0200

    Remove unused imports
    Signed-off-by: Sylvain Leclerc <sylvain.leclerc@rte-france.com>

--- src/main/java/com/powsybl/diffserver/DiffStudyApplication.java
+++ src/main/java/com/powsybl/diffserver/DiffStudyApplication.java
@@ -8,9 +8,6 @@ package com.powsybl.diffserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * @author Christian Biasuzzi <christian.biasuzzi@techrain.eu>
